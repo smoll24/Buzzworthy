945aeffde254b7860a06208938d6b1a4a544d256
Sam Moll <115204665+smoll24@users.noreply.github.com>
2025-06-03 03:53:58 -0700

    Minor tweaks

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -5,8 +5,8 @@ var max_health = 5
 var current_health = max_health
 var current_dialog = 1
 var slow = false
-var logs = 10
-var stones = 10
+var logs = 0
+var stones = 0
 var crafting = true
 
 var save_pos = Vector2(0, 0)
@@ -16,13 +16,13 @@ var max_wet = 5
 
 var current_level = 1
 
-var has_sword_recipe = true
+var has_sword_recipe = false
 var has_sword = false
-var has_shield_recipe = true
+var has_shield_recipe = false
 var has_shield = false
-var has_helmet_recipe = true
+var has_helmet_recipe = false
 var has_helmet = false
-var has_boots_recipe = true
+var has_boots_recipe = false
 var has_boots = false
 
 #Trigger to exit cutscene
